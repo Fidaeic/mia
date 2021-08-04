5e1baef6e464095939c70c86e26d06837c1d2474
fidae <fidae_em@gis4tech.com>
2021-08-04 19:09:23 +0200

    Added descriptions of methods

--- src/MIAPCA.py
+++ src/MIAPCA.py
@@ -14,18 +14,47 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
 
-class PCAMIA():
+class MIAPCA():
     def __init__(self, path_to_image):
+        '''
+        This class is intended for analyzing multiband images through color-texture analysis.
+        It extends a multiband image batchwise and applies a PCA for dimensionality reduction
+        and feature extraction
+
+        Parameters
+        ----------
+        path_to_image : string
+            Path to the image to be analyzed.
+
+        '''
         self.path_to_directory = path_to_image
         
        
     def read_image(self):
+        '''
+        Reads image from path and stores it as an attribute. The method also 
+        takes the number of rows and columns as attributes
+
+        Returns
+        -------
+        Original image as numpy ndarray
+        Number of rows and columns.
+
+        '''
         
         self.image = rio.open(self.path_to_directory).read()
         self.rows = self.image.shape[1]
         self.columns = self.image.shape[2]
         
     def plot_image(self):
+        '''
+        Plots original image
+
+        Returns
+        -------
+        RGB image.
+
+        '''
         
         show(self.image)
         
@@ -46,14 +75,19 @@ class PCAMIA():
             numpy ndarray with the bands and the neighbouring pixels extended batchwise
         '''
 
+        # Get the number of bands of the image
         depth = self.image.shape[0]
+        
+        # Create an empty array that will contain the original image extended batchwise
         
         final_matrix = np.array([])
         
         for d in range(depth):
             
+            # Apply the window function to each band of the original image
             wind = utils.window(self.image[d, :, :], neighbours)
             
+            # Append the extended band to the final matrix
             if final_matrix.shape[0]==0:
                 final_matrix = wind
             else:
@@ -64,6 +98,27 @@ class PCAMIA():
         self.new_columns = self.columns-2*neighbours
     
     def reduce(self, ncomps):
+        '''
+        Applies PCA to the image after extending it batchwise
+
+        Parameters
+        ----------
+        ncomps : int
+            Number of components to be considered for the PCA.
+
+        Returns
+        -------
+        Scores: numpy ndarray
+            Coordinates of the extended image after projecting it on a reduced
+            subspace
+        Loadings: numpy ndarray
+            Set of vectors that define the directions of the principal components
+            in the new subspace
+        Rsquare: list
+            Accumulated explained variance for each component
+
+        '''
+        
         
         self.scores, self.loadings, self.rsquare = utils.nipals(X=self.extended_image, 
                                ncomps=ncomps, 
@@ -75,7 +130,21 @@ class PCAMIA():
         print(self.rsquare)
     
     def score_map(self, component):
-    
+        '''
+        2D map that represents the scores of the specified principal component. When compared,
+        score maps of different principal components are very useful for feature extraction
+
+        Parameters
+        ----------
+        component : int
+            Principal component to display.
+
+        Returns
+        -------
+        Matplotlib plot with the score map of the specified component.
+
+        '''
+        # Reshape the score column for the specified component to fit the new shape of the image
         score_plot = np.reshape(self.scores[:, component], newshape=(self.new_rows, self.new_columns))
     
         plt.imshow(score_plot, interpolation='bilinear', cmap='RdBu')
@@ -87,12 +156,31 @@ class PCAMIA():
 #    def loadings_plot(self):
         
     def clusters(self, n_clusters):
+        '''
+        Applies a clustering method to the scores matrix after reducing the extended
+        image. Currently only supports KMeans.
+
+        Parameters
+        ----------
+        n_clusters : int
+            Number of clusters to generate.
+
+        Returns
+        -------
+        Matplotlib plot with a cluster map.
+
+        '''
+        
+        # Create the cluster object with the specified number of clusters
+        # and fit it to the score matrix
         knn = KMeans(n_clusters)
     
         knn.fit(self.scores)
         
+       
         self.clusters_labels = knn.labels_
         
+        # Create a 2d array where every pixel is represented with a cluster number        
         cluster_map= np.reshape(self.clusters_labels, newshape=(self.new_rows, self.new_columns))
         
         cmap = plt.get_cmap("RdBu", n_clusters)
@@ -104,6 +192,20 @@ class PCAMIA():
         plt.show()
 #        
     def scatter_scores(self, component_1, component_2):
+        '''
+        Creates a scatter plot to compare the score of two components. May be useful
+        to detect directions of variability and clusters
+
+        Parameters
+        ----------
+        component_1, component_2 : int
+            Principal components to display.
+
+        Returns
+        -------
+        Matplotlib plot with a scatter plot comparing the scores of 2 principal components.
+
+        '''
     
         plt.scatter(self.scores[:, component_1], self.scores[:, component_2])
         
@@ -113,7 +215,5 @@ class PCAMIA():
         plt.title(f"Score plot for components {component_1+1} and {component_2+1}")
         
         plt.show()
-#        
-#    def save_image(self):
         
 
